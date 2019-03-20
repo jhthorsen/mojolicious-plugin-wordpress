96aa7e4642d2905f74314d2ce52281c136cbb0ae
Jan Henning Thorsen <jhthorsen@cpan.org>
2019-03-20 22:21:37 +0800

    Add documentation

--- lib/Mojolicious/Plugin/Wordpress.pm
+++ lib/Mojolicious/Plugin/Wordpress.pm
@@ -10,7 +10,7 @@ use constant DEBUG => $ENV{MOJO_WORDPRESS_DEBUG} || 0;
 our $VERSION = '0.01';
 
 has base_url       => 'http://localhost/wp-json';     # Will become a Mojo::URL object
-has processor      => undef;
+has post_processor => undef;
 has ua             => sub { Mojo::UserAgent->new };
 has yoast_meta_key => 'yoast_meta';
 
@@ -18,7 +18,7 @@ sub register {
   my ($self, $app, $config) = @_;
   my $prefix = $config->{prefix} || 'wp';
 
-  $self->$_($config->{$_}) for grep { $config->{$_} } qw(base_url processor yoast_meta_key ua);
+  $self->$_($config->{$_}) for grep { $config->{$_} } qw(base_url post_processor yoast_meta_key ua);
   $self->base_url(Mojo::URL->new($self->base_url)) unless ref $self->base_url;
 
   $app->helper("$prefix.meta_from" => sub { $self->_helper_meta_from(@_) });
@@ -66,13 +66,13 @@ sub _helper_get_post_p {
   $params = {slug => $params} unless ref $params;
 
   my %query = %$params;
-  delete $params->{processor};
+  delete $params->{post_processor};
 
-  my $processor = $params->{processor} || $self->processor;
+  my $processor = $params->{post_processor} || $self->post_processor;
   return $self->_raw(get_p => "wp/v2/$type", \%query)->then(sub {
     my $wp_res = shift->res;
     my $post   = _arr($wp_res->json)->[0];
-    return $post && $processor ? $self->$processor($post) : $post;
+    return $post && $processor ? $c->$processor($post) : $post;
   });
 }
 
@@ -80,11 +80,11 @@ sub _helper_get_posts_p {
   my ($self, $type, $c, $params) = @_;
 
   my %query = %{$params || {}};
-  delete $query{$_} for qw(all processor);
+  delete $query{$_} for qw(all post_processor);
   $query{page}     = 1   if $params->{all};
   $query{per_page} = 100 if $params->{all} and !$query{per_page};
 
-  my $processor = $params->{processor} || $self->processor;
+  my $processor = $params->{post_processor} || $self->post_processor;
   my ($gather, @posts);
   $gather = sub {
     my $wp_res = shift->res;
@@ -119,3 +119,80 @@ sub _raw {
 }
 
 1;
+
+=encoding utf8
+
+=head1 NAME
+
+Mojolicious::Plugin::Wordpress - Use Wordpress as a headless CMS
+
+=head1 SYNOPSIS
+
+  use Mojolicious::Lite;
+  plugin wordpress => {base_url => "https://wordpress.example.com/wp-json"};
+
+  get "/page/:slug" => sub {
+    my $c = shift->render_later;
+    $c->wp->get_page_p($c->stash("slug"))->then(sub {
+      my $page = shift;
+      $c->render(json => $page);
+    });
+  };
+
+=head1 DESCRIPTION
+
+L<Mojolicious::Plugin::Wordpress> is a plugin for getting data using the
+Wordpress JSON API.
+
+=head1 ATTRIBUTES
+
+=head2 base_url
+
+  my $url = $wp->base_url;
+  my $wp  = $wp->base_url("https://wordpress.example.com/wp-json");
+
+Holds the base URL to the Wordpress server, including "/wp-json".
+
+=head2 processor
+
+  my $cb = $wp->processor;
+  my $wp = $wp->processor(sub { my ($c, $post) = @_ });
+
+A code block that can be used to post process the JSON response from Wordpress.
+
+=head2 ua
+
+  my $ua = $wp->ua;
+  my $wp = $wp->ua(Mojo::UserAgent->new);
+
+Holds a L<Mojo::UserAgent> object that is used to get data from Wordpress.
+
+=head2 yoast_meta_key
+
+  my $str = $wp->yaost_meta_key;
+  my $wp  = $wp->yaost_meta_key("yoast_meta");
+
+The key in the post JSON response that holds YOAST meta information.
+
+=head1 METHODS
+
+=head2 register
+
+  $wp->register($app, \%config);
+  $app->plugin(wordpress => \%config);
+
+Used to register this plugin. Each key in C<%config> that matches
+L</ATTRIBUTES> will be used as an attribute.
+
+=head1 AUTHOR
+
+Jan Henning Thorsen
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C), Jan Henning Thorsen.
+
+This program is free software, you can redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
+
+=cut
